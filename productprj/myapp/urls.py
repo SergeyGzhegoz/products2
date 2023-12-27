f1dffbcc4c48d502c4ff93243ba656f0a0e8691e
SergeyGzhegoz <s.gz@yandex.ru>
2023-12-27 16:38:59 +0300

    Добавил формы добавления Клиета, Товара

--- productprj/myapp/urls.py
+++ productprj/myapp/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('order_form', views.order_form),
+    path('prod_form', views.prod_form),
+    path('cust_form', views.cust_form),
 ]
--- productprj/myapp/views.py
+++ productprj/myapp/views.py
@@ -1,8 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Orders
+from .forms import OrderForm, ProdForm, CustForm
 
 
 def index(request):
     orders_all = Orders.objects.order_by('id')
     return render(request, 'myapp/index.html', {'title': 'Главная страница сайта',
-                                               'tasks': orders_all})
+                                                'tasks': orders_all})
+
+
+def order_form(request):
+    error = ""
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Форма была неверной'
+
+    form = OrderForm()
+    context = {'form': form, 'error': error}
+    return render(request, 'myapp/order_form.html', context)
+
+
+def prod_form(request):
+    error = ""
+    if request.method == 'POST':
+        form = ProdForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Форма была неверной'
+
+    form = ProdForm()
+    context = {'form': form, 'error': error}
+    return render(request, 'myapp/prod_form.html', context)
+
+
+def cust_form(request):
+    error = ""
+    if request.method == 'POST':
+        form = CustForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Форма была неверной'
+
+    form = CustForm()
+    context = {'form': form, 'error': error}
+    return render(request, 'myapp/cust_form.html', context)
