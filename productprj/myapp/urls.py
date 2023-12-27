a2d9bc8a4e7f96ec6a687215a26f5b0f8dd01bef
SergeyGzhegoz <s.gz@yandex.ru>
2023-12-27 15:35:01 +0300

    --

--- productprj/myapp/urls.py
+++ productprj/myapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    # path('', include('myapp.urls')),
+    path('', views.index, name='home'),
 ]
--- productprj/myapp/views.py
+++ productprj/myapp/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from models import Orders
+from .models import Orders
+
 
 def index(request):
     orders_all = Orders.objects.order_by('id')
-    return render(request, 'main/index.html', {'title': 'Главная страница сайта',
+    return render(request, 'myapp/index.html', {'title': 'Главная страница сайта',
                                                'tasks': orders_all})
